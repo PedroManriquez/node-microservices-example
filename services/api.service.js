dabde15bafa6cdccc0311446f8d3396960678aeb
PedroManriquez <ptrmanriquez@gmail.com>
2018-11-14 15:45:56 -0300

    Setup inicial, generado por moleculer-cli

---
+++ services/api.service.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const ApiGateway = require("moleculer-web");
+
+module.exports = {
+	name: "api",
+	mixins: [ApiGateway],
+	// More info about settings: https://moleculer.services/docs/0.13/moleculer-web.html
+	settings: {
+		port: process.env.PORT || 3000,
+    cors: { // Global CORS settings for all routes
+      origin: "*",
+      methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+      allowedHeaders: [],
+      exposedHeaders: [],
+      credentials: false
+    },
+		routes: [{
+			path: "/api",
+			whitelist: [
+				// Access to any actions in all services under "/api" URL
+				"**"
+			],
+			aliases: {
+				"GET countries": "countries.index",
+				"POST countries": "countries.create",
+				"GET users": "users.index",
+        "POST users": "users.create"
+			}
+		}],
+
+		// Serve assets from "public" folder
+		/* assets: {
+			folder: "public"
+		} */
+	}
+};
